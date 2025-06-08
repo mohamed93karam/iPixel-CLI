1fd31ea91cf47760da00e1fa5abeb6a2869b53e4
lucagoc <77584602+lucagoc@users.noreply.github.com>
2025-06-08 11:30:52 +0200

    [Engine] Ability to run multiple -c commands

--- ipixelcli.py
+++ ipixelcli.py
@@ -72,21 +72,36 @@ async def start_server(ip, port, address):
     print(f"WebSocket server started on ws://{ip}:{port}")
     await server.wait_closed()
 
+def build_command_args(params):
+    positional_args = []
+    keyword_args = {}
+    for param in params:
+        if "=" in param:
+            key, value = param.split("=", 1)
+            keyword_args[key.replace('-', '_')] = value
+        else:
+            positional_args.append(param)
+    return positional_args, keyword_args
+
+async def run_multiple_commands(commands, address):
+    async with BleakClient(address) as client:
+        print("[INFO] Connected to the device")
+        for cmd in commands:
+            command_name = cmd[0]
+            params = cmd[1:]
+            if command_name in COMMANDS:
+                positional_args, keyword_args = build_command_args(params)
+                data = COMMANDS[command_name](*positional_args, **keyword_args)
+                await client.write_gatt_char("0000fa02-0000-1000-8000-00805f9b34fb", data)
+                print(f"[INFO] Command '{command_name}' executed successfully.")
+            else:
+                print(f"[ERROR] Unknown command: {command_name}")
+
 async def execute_command(command_name, params, address):
     async with BleakClient(address) as client:
         print("[INFO] Connected to the device")
         if command_name in COMMANDS:
-            # Separate positional and keyword arguments
-            positional_args = []
-            keyword_args = {}
-            for param in params:
-                if "=" in param:
-                    key, value = param.split("=", 1)
-                    keyword_args[key.replace('-', '_')] = value
-                else:
-                    positional_args.append(param)
-
-            # Call the command with both positional and keyword arguments
+            positional_args, keyword_args = build_command_args(params)
             data = COMMANDS[command_name](*positional_args, **keyword_args)
             await client.write_gatt_char("0000fa02-0000-1000-8000-00805f9b34fb", data)
             print(f"[INFO] Command '{command_name}' executed successfully.")
@@ -97,8 +112,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="WebSocket BLE Server")
     parser.add_argument("-s", "--server", action="store_true", help="Run as WebSocket server")
     parser.add_argument("-p", "--port", type=int, default=4444, help="Specify the port for the server")
-    parser.add_argument("-c", "--command", nargs="+", metavar="COMMAND PARAMS",
-                        help="Execute a specific command with parameters")
+    parser.add_argument(
+        "-c", "--command", action="append", nargs="+", metavar="COMMAND PARAMS",
+        help="Execute a specific command with parameters. Can be used multiple times."
+    )
     parser.add_argument("-a", "--address", required=True, help="Specify the Bluetooth device address")
 
     args = parser.parse_args()
@@ -106,8 +123,6 @@ if __name__ == "__main__":
     if args.server:
         asyncio.run(start_server("localhost", args.port, args.address))
     elif args.command:
-        command_name = args.command[0]
-        params = args.command[1:]
-        asyncio.run(execute_command(command_name, params, args.address))
+        asyncio.run(run_multiple_commands(args.command, args.address))
     else:
         print("[ERROR] No mode specified. Use --server or -c with -a to specify an address.")
